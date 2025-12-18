bcdc8924cc5fc443ec48c74f51803ed02e86a750
jonathan <jonathanjavier2004@hotmail.com>
2025-12-18 12:17:47 -0500

    avance

--- Taller10/src/paquete6/Problema06.java
+++ Taller10/src/paquete6/Problema06.java
@@ -5,10 +5,27 @@
  */
 package paquete6;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema06 {
 
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int[][] ingreso = new int[5][6];
+        int[][] salida = new int[5][6];
+        int multi;
+        System.out.println("ingrese el multiplicador");
+        multi = entrada.nextInt();
+         for (int f = 0; f < ingreso.length; f++) {
+            for (int c = 0; c < ingreso[f].length; c++) {
+                System.out.printf("Ingrese los valores de A en la posicion %s %s\n", f, c);
+                matA[f][c] = entrada.nextInt();
+                
+            }
+            }
+    }
 }
