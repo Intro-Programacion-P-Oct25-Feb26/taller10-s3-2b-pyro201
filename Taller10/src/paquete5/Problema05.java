f07da9bf439782e66e9172ea150a70c700bf4f80
jonathan <jonathanjavier2004@hotmail.com>
2025-12-18 12:11:04 -0500

    problema05

--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -10,5 +10,30 @@ package paquete5;
  * @author reroes
  */
 public class Problema05 {
-    
+
+    public static void main(String[] args) {
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+        {"Carrie Burton", "Lauren Rice"},
+        {"Emma Escobar", "Lori Flores"},
+        {"Steven West", "Toni Martin"}
+        };
+        String inicial;
+        String report = "";
+
+        for (int f = 0; f < estudiantes.length; f++) {
+            for (int c = 0; c < estudiantes[f].length; c++) {
+                String nombre = estudiantes[f][c];
+                inicial = nombre.substring(0, 1);
+                switch (inicial) {
+                    case ("S"):
+                    case ("P"):
+                    case ("T"):
+                        report+= String.format("%s\n",nombre);
+                        break;
+                }
+
+            }
+        }
+        System.out.println(report);
+    }
 }
